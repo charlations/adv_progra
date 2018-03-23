da45a4bcadd8eec60ebd6a19ed0029c81e191512
CarlaH <carla.herrera.rendon@gmail.com>
2018-03-23 09:55:40 -0600

    notas de server en socket

--- exercises/sockets/server.c
+++ exercises/sockets/server.c
@@ -30,6 +30,9 @@ void serves_client(int nsfd) {
 }
 
 void server(char* ip, int port, char* program) {
+	/* Estos pasos siempre son los mismos, porque el protocolo de comunicación 
+	casi no cambia. (?) Nos mostró que en su tesis, igual usa un proceso muy
+	igual, solo cambió porque está escrito en python. */
 	int sfd, nsfd, pid;
 	struct sockaddr_in server_info, client_info;
 
@@ -40,16 +43,34 @@ void server(char* ip, int port, char* program) {
 	
 	server_info.sin_family = AF_INET;
 	server_info.sin_addr.s_addr = inet_addr(ip);
+	/* 															ⴿ
+									conjunto de familia de funciones que traducen la ip a bits
+									inet cambia de string (ip de 4 puntos o dominio) a bits 
+									También hay una función que traduce de bits a ip de 4
+									puntos, hace la petición de DNS y si tiene dominio, regresa 
+	*/
 	server_info.sin_port = htons(port);
+	/* Little endian o big endian traducido a representación de network 
+			Hardware TO Network System
+	*/
 	if ( bind(sfd, (struct sockaddr *) &server_info, sizeof(server_info)) < 0 ) {
+		/* Los errores suelen ser si se usan puertos de 0 - 1023, a menos de que
+				sea el root quien lo solicita. */
 		perror(program);
 		exit(-1);
 	}
 	
-	listen(sfd, 1);
+	listen(sfd, 1); /* (socket, máximo de fila de espera)
+											TCP socket error 87 - too many users 
+											HTTP error 503 - server too busy */
 	while (1) {
 		int len = sizeof(client_info);
 		if ( (nsfd = accept(sfd, (struct sockaddr *) &client_info, &len)) < 0 ) {
+			/* accept es el único punto donde el servidor puede obtener la
+				 información del cliente. (Luego se puede solicitar información
+				 extra, pero el protocolo es ese. ) */
+			/* New Socket File Descriptor, porque el anterior solo se usa para
+				 escuchar */
 			perror(program);
 			exit(-1);
 		}
@@ -59,10 +80,13 @@ void server(char* ip, int port, char* program) {
 			perror(program);
 		} else if (pid == 0) {
 			close(sfd);
+			/* El proceso hijo no debe estar escuchando para nuevas conexiones, 
+				 entonces lo cierra para él. */
 			serves_client(nsfd);
 			exit(0);
 		} else {
 			close(nsfd);
+			/* El servidor no va a atender al cliente, entonces él cierra el nsfd */
 		}
 		/*CONCURRENTE */
 		
